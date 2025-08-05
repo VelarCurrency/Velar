4dcd79f050e173afd49bb69cb4668b16200639ed
ternicinc <allexander.linux@gmail.com>
2025-08-05 13:47:33 +0200

    Default

---
+++ src/main.pas
@@ -0,0 +1,36 @@
+program VelarCurrency;
+
+uses
+  SysUtils;
+
+
+function CopyrightNote(username: string): string;
+begin
+  CopyrightNote := 'Velar Currency System - Copyright (c) 2025 by ' + username + '. All rights reserved.';
+end;
+
+var
+  username: string;
+
+begin
+
+    username := 'Allexander Bergmans';
+    WriteLn(CopyrightNote(username));
+    WriteLn('--------------------------------------------');
+
+    Sleep(4000);
+
+    WriteLn('Velar Currency System Initialized');
+    WriteLn('This program is designed to handle Velar Currency transactions.');
+    
+    WriteLn('Deamon is currently syncing with the blockchain...');
+    WriteLn('Please wait while we establish a secure connection...');
+    Sleep(2000); 
+    WriteLn('Connection established successfully.');
+    WriteLn('Currently syncing blocks...');
+    Sleep(3000);
+    WriteLn('Sync complete. You can now perform node operations.');
+    WriteLn('Welcome to the Velar Currency Node!');
+    WriteLn('For assistance, please refer to the documentation or contact support.');
+    
+end.
